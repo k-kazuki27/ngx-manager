af9f822b9abea94bc227a3ecddf7fe750a6105fd
kazuki.kawabata <kazuki.kawabata@fullmarks.co.jp>
2018-08-27 22:22:52 +0900

    close nav

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,7 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material';
+import { NavigationEnd, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -12,6 +13,8 @@ import { map } from 'rxjs/operators';
 })
 export class PagesComponent implements OnInit {
 
+  @ViewChild(MatSidenav) sidenav: MatSidenav;
+
   loginUserName = 'ログインユーザ';
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
@@ -19,8 +22,15 @@ export class PagesComponent implements OnInit {
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver,
-    private router: Router) { }
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router) {
+    this.router.events.subscribe(val => {
+      if (val instanceof NavigationEnd && this.sidenav.mode === 'over') {
+        this.sidenav.toggle();
+      }
+    });
+  }
 
   ngOnInit() {
   }
