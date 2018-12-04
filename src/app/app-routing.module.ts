05564b9d8123a53fe5690da7eb16245213c3276a
kazuki.kawabata <>
2018-12-04 23:51:30 +0900

    modify

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-import { AuthGuard } from './shared/guard/auth.guard';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AuthGuard} from './core/guard/auth.guard';
 
 const routes: Routes = [
-  { path: 'login', loadChildren: './login/login.module#LoginModule' },
-  { path: '', loadChildren: './pages/pages.module#PagesModule', canActivate: [AuthGuard] },
-  { path: '**', redirectTo: '' }
+  {path: 'login', loadChildren: './login/login.module#LoginModule'},
+  {
+    path: '',
+    loadChildren: './pages/pages.module#PagesModule',
+    canActivate: [AuthGuard]
+  },
+  {path: '**', redirectTo: ''}
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes,
-    {
+  imports: [
+    RouterModule.forRoot(routes, {
       useHash: true,
       onSameUrlNavigation: 'reload',
       scrollPositionRestoration: 'enabled', // ブラウザバックしたときに遷移前のスクロール位置に復元する
       anchorScrolling: 'enabled' // アンカーリンク有効
-    }
-  )],
+    })
+  ],
   exports: [RouterModule],
-  providers: [AuthGuard]
+  providers: []
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
